af574448b0ef3f0f4ae9bcc23850d74f5cc67a29
Dylan3871 <al222010139@gmail.com>
2021-10-15 12:10:35 -0500

    Proyecto Componentes

---
+++ android/app/src/main/java/com/example/componentes/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.componentes;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+
+}
